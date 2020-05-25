22b4222254291fb967ac4dc01bc39cf06ce94a95
Anthony <nguyenanthony80@gmail.com>
2020-05-25 14:57:58 -0700

    Updates

---
+++ precache-manifest.eeb5a1454ae11d4cde489810a8523f99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577c9087cec15e219fb41e9f3d22f143",
+    "url": "/react-rouge/index.html"
+  },
+  {
+    "revision": "e37710eca0f3ced0649c",
+    "url": "/react-rouge/static/js/2.96c2a986.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rouge/static/js/2.96c2a986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72684148a3e5d567d032",
+    "url": "/react-rouge/static/js/main.3aeb240d.chunk.js"
+  },
+  {
+    "revision": "4ec768fecad5d5bf50cf",
+    "url": "/react-rouge/static/js/runtime-main.c30f98d3.js"
+  }
+]);
